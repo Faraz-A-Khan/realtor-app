e2e2ba491be47c01f86b93223092708bda13c370
Faraz A. Khan <official.farazahmedkhan@gmail.com>
2025-05-28 00:35:12 +0500

    Until Json Web Tokens

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { SignupDto } from '../dto/auth.dto';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from 'generated/prisma';
+
+interface signupParams {
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+}
 
 @Injectable()
 export class AuthService {
-  signup(body: SignupDto) {
-    return body;
+  constructor(private readonly prismaService: PrismaService) { }
+  async signup({ name, phone, email, password }: signupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email: email
+      }
+    });
+    if (userExists) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    if (!hashedPassword) {
+      throw new ConflictException('Error hashing password');
+    }
+    console.log('hashedPassword', hashedPassword);
+    const user = await this.prismaService.user.create({
+      data: {
+        email: email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      }
+    });
+    return user;
   }
 }
