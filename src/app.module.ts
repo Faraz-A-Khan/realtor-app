4944304cff7e37c1e35e37951c2d9a5320a69458
Faraz A. Khan <official.farazahmedkhan@gmail.com>
2025-05-29 14:50:19 +0500

    Few Changes to env variable config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true, // makes ConfigService available app-wide
-  }), UserModule, PrismaModule],
+  imports: [UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
